a659bb66043dc2ff72556f6f75baaa2ee7d00177
PSheon <pauljiang61020@gmail.com>
2024-01-11 16:38:24 +0800

    feat: updated blurhash plugin config.

---
+++ config/env/development/plugins.ts
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "local",
+      sizeLimit: 25 * 1024 * 1024,
+    },
+  },
+  "strapi-blurhash": {
+    enabled: true,
+    config: {
+      regenerateOnUpdate: true,
+    },
+  },
+});
